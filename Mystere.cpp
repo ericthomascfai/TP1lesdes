67a266432d5e0cae4c263d7d613b15ad0139c0f2
eric <eric.thomas@formation-industries-fc.fr>
2021-09-03 13:49:49 +0200

    1.1 1.2 1.3 1.4 1.5

--- Mystere.cpp
+++ Mystere.cpp
@@ -1,5 +1,23 @@
 //
 // Created by eric on 03/09/2021.
 //
+using namespace std;
 
+#include <iostream>
 #include "Mystere.h"
+
+int Mystere::saisiejoueur() {
+    int saisie=0;
+    cout<<"Veuillez saisir un nombre entre 1 et 100"<<endl;
+    cin>>saisie;
+    return saisie;
+}
+
+void Mystere::jeu() {
+
+}
+
+Mystere::Mystere() {
+de100=De100(); //instanciation de de100
+nbcoupsmax=10; //assignation du nombre de coups
+}
